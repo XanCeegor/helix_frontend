2a235612f302ab4ca70d4b733e6d6a38c029071a
Alex <alex.gouws@gmail.com>
2020-01-06 09:16:53 +0200

    changed toolbar title to use name from .env file

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
     <v-app-bar color="purple darken-3" dark>
-      <!-- <v-toolbar-title class="font-weight-thin"><router-link :to="{name: 'home'}">Helix</router-link></v-toolbar-title> -->
       <v-spacer></v-spacer>
-      <v-toolbar-title class="font-weight-thin display-1">Helix</v-toolbar-title>
+      <v-toolbar-title class="font-weight-thin display-1">{{title}}</v-toolbar-title>
       <div class="flex-grow-1"></div>
     </v-app-bar>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    title: process.env.VUE_APP_TITLE
+  })
+};
 </script>
-
-<style>
-</style>
